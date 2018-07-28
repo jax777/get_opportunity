22d66cbf02a1ac077fa047800b3d3369759322be
rasca1 <635713319@qq.com>
2018-07-28 11:56:30 +0800

    test

--- stock_monitor.py
+++ stock_monitor.py
@@ -4,13 +4,85 @@ Created on 2018年7月28日
 
 @author: guimaizi
 '''
-import json
+import json,time,datetime
+import tushare as ts
 class stock_monitor:
     def __init__(self):
         #股价监测
         pass
-    def read_config(self):
-        #返回配置文件信息
-        with open(r"stock_list.json",'r') as load_f:
-            load_dict = json.load(load_f)
-        return load_dict
+    def start(self):
+        try:
+            while True:
+                times = datetime.datetime.now()
+                if times.hour in [9,10,11,13,14]:
+                    #返回配置文件信息
+                    with open(r"stock_list.json",'r') as load_f:
+                        load_dict = json.load(load_f)
+                    for i in load_dict['stock_list']:
+                        self.mains(i)
+                    time.sleep(10)
+                else:
+                    time.sleep(60)
+        except Exception as e:
+            pass
+    def mains(self,stock_code):
+        df = ts.get_realtime_quotes(stock_code) #Single stock symbol
+        dfs = ts.get_stock_basics()
+        data=str(df[['name','pre_close','price','amount']]).split()
+        #print(data)
+        money=int(float(data[-1]))
+        if int(money)>5:
+            Amount=round(money/10000,0)*10000
+        else:
+            Amount=money
+        change=round((float(data[7])-float(data[6]))/float(data[6])*100,2)
+        if change>5 or change<-5:
+            tests='股票: %s 当前价格:  %s ,涨跌幅: %%%s ,成交量:  %s,时间: %s'%(data[5],data[7],change,self.to_chinese(int(Amount)),datetime.datetime.now())
+            print(tests)
+        #totals=(dfs.ix[stock_code]['totals'])
+        #print(float(data[7])*totals)
+    def to_chinese(self,number):
+        """ convert integer to Chinese numeral """
+        chinese_numeral_dict = {
+            '0': '零',
+            '1': '一',
+            '2': '二',
+            '3': '三',
+            '4': '四',
+            '5': '五',
+            '6': '六',
+            '7': '七',
+            '8': '八',
+            '9': '九'
+        }
+        chinese_unit_map = [('', '十', '百', '千'),
+                            ('万', '十万', '百万', '千万'),
+                            ('亿', '十亿', '百亿', '千亿'),
+                            ('兆', '十兆', '百兆', '千兆'),
+                            ('吉', '十吉', '百吉', '千吉')]
+        chinese_unit_sep = ['万', '亿', '兆', '吉']
+        reversed_n_string = reversed(str(number))
+        result_lst = []
+        unit = 0
+        for integer in reversed_n_string:
+            if integer is not '0':
+                result_lst.append(chinese_unit_map[unit // 4][unit % 4])
+                result_lst.append(chinese_numeral_dict[integer])
+                unit += 1
+            else:
+                if result_lst and result_lst[-1] != '零':
+                    result_lst.append('零')
+                unit += 1
+        result_lst.reverse()
+        if result_lst[-1] is '零':
+            result_lst.pop()
+        result_lst = list(''.join(result_lst))
+        for unit_sep in chinese_unit_sep:
+            flag = result_lst.count(unit_sep)
+            while flag > 1:
+                result_lst.pop(result_lst.index(unit_sep))
+                flag -= 1
+        return ''.join(result_lst)
+if __name__=='__main__':
+    itme=stock_monitor()
+    itme.start()
--- test.py
+++ test.py
@@ -4,14 +4,7 @@ Created on 2018年7月27日
 
 @author: guimaizi
 '''
-import json,requests,time,datetime
-def get_lunar(data):
-    url = "https://www.sojson.com/open/api/lunar/json.shtml?date=%s"%(data)
-    time.sleep(3)
-    data_lunar = json.loads(requests.get(url).text)
-    lunar_year = data_lunar['data']['lunarYear']
-    lunar_month = data_lunar['data']['lunarMonth']
-    lunar_day = data_lunar['data']['lunarDay']
-    # print(data_lunar['data'])
-    return lunar_year,lunar_month,lunar_day
-print(get_lunar(2018/7/28))
+num=5.1
+print(type(num))
+if num>5 or num<-5:
+    print(1111)
